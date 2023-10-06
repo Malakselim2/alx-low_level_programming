241b484a17f1a9a82c05195302b9c68f63337a33
Malak <malaksleem3@gmail.com>
2023-10-06 02:57:22 -0700

    mee

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	return (0);
+}
