29bd1dc8dc812f76433e69d410f758f64b75bdf7
Malak <malaksleem3@gmail.com>
2023-10-10 23:00:02 -0700

    g

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - Test function for positive or negative
+* Return: 0
+*/
+int main(void)
+{
+	int i=0;
+	
+	if (i<0)
+	{
+		printf("i is negative");
+	}
+	else
+	{
+		printf("i is positive");
+	}
+	return (0);
+}
