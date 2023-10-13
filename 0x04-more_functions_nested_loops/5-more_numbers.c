30738ff11bdfd3b0832ea9da3c290d79c21cfce4
Malak <malaksleem3@gmail.com>
2023-10-13 03:21:15 -0700

    g

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,18 @@
 
 void more_numbers(void)
 {
-	char c;
+	char i, j;
 
-	for (c = '0'; c <= '9'; c++)
+	for (i = '0'; i <= '10'; i++)
 	{
-		_putchar(c * 10);
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n')
 }
